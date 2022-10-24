344f255582f1cd0540716ee024a42b746a0e9d16
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-10-24 20:11:48 -0300

    feat: create app and config sceneDelegate

---
+++ escoleta/escoleta/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  escoleta
+//
+//  Created by Grazi  Berti on 24/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
