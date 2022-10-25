198ed3331d385ebc3e5e05b886be3b70a2ea02e9
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-10-25 09:01:22 -0300

    feat: create navigation to ChooseCategoryViewController

---
+++ escoleta/escoleta/Controller/ChooseCategoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseCategoryViewController.swift
+//  escoleta
+//
+//  Created by Grazi  Berti on 25/10/22.
+//
+
+import UIKit
+
+class ChooseCategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .backgroundColor
+    }
+
+}
--- escoleta/escoleta/Resources/SceneDelegate.swift
+++ escoleta/escoleta/Resources/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
         
-        let viewController = ViewController()
+        let viewController = HomeViewController()
         let navigation = UINavigationController(rootViewController: viewController)
         
         window.rootViewController = navigation
