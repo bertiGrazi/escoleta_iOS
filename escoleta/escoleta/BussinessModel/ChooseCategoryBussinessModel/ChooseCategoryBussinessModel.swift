8da1119be119cb9209d17f3821e2919f55211a9e
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-10-27 22:35:07 -0300

    update ChooseCategoryBussinessModel

--- escoleta/escoleta/BussinessModel/ChooseCategoryBussinessModel/ChooseCategoryBussinessModel.swift
+++ escoleta/escoleta/BussinessModel/ChooseCategoryBussinessModel/ChooseCategoryBussinessModel.swift
@@ -15,7 +15,7 @@ class ChooseCategoryBussinessModel: ChooseCategoryProtocol {
             ChooseCategoryData(iconCategory: UIImage(named: "electronics") ?? UIImage(), nameCategory: "Resíduos\nEletrônicos"),
             ChooseCategoryData(iconCategory: UIImage(named: "lamps") ?? UIImage(), nameCategory: "Lâmpadas"),
             ChooseCategoryData(iconCategory: UIImage(named: "oil") ?? UIImage(), nameCategory: "Óleo de Cozinha"),
-            ChooseCategoryData(iconCategory: UIImage(named: "organic") ?? UIImage(), nameCategory: "Resíduos Orgânicos"),
+            ChooseCategoryData(iconCategory: UIImage(named: "organic") ?? UIImage(), nameCategory: "Resíduos\nOrgânicos"),
             ChooseCategoryData(iconCategory: UIImage(named: "paperAndCardboard") ?? UIImage(), nameCategory: "Papéis e Papelão"),
         ]
     }
