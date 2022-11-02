ae667b4cd3d68c709d9872370d7c3f36f5313396
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-11-02 10:56:42 -0300

    settings tableView Cell for places found

--- escoleta/escoleta/Controller/PlacesList/PlacesListCell.swift
+++ escoleta/escoleta/Controller/PlacesList/PlacesListCell.swift
@@ -18,21 +18,26 @@ class PlacesListCell: UITableViewCell {
         stackView.axis = .vertical
         stackView.backgroundColor = .white
         stackView.distribution = .fill
+        stackView.spacing = 2
+        stackView.layer.cornerRadius = 20
+        stackView.clipsToBounds = true
+        stackView.backgroundColor = .backgroundColor
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
     fileprivate let placeImageView: UIImageView = {
         let imageview = UIImageView()
-        imageview.contentMode = .scaleAspectFit
-        imageview.layer.cornerRadius = 12
+        imageview.contentMode = .scaleAspectFill
+        imageview.layer.cornerRadius = 20
+        imageview.clipsToBounds = true
         imageview.translatesAutoresizingMaskIntoConstraints = false
         return imageview
     }()
     
     fileprivate let namePlaceLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.font = UIFont.boldSystemFont(ofSize: 32)
         label.textColor = .titleColor
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -40,15 +45,39 @@ class PlacesListCell: UITableViewCell {
     
     fileprivate let categoryLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.font = UIFont.boldSystemFont(ofSize: 24)
         label.textColor = .buttonPrimaryColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    fileprivate let adressLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .subtitleColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    fileprivate let neighborhoodLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .subtitleColor
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     fileprivate let stateLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 10)
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .subtitleColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    fileprivate let numberLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
         label.textColor = .subtitleColor
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -69,38 +98,51 @@ class PlacesListCell: UITableViewCell {
     private func setupView() {
         namePlaceLabel.numberOfLines = 0
         categoryLabel.numberOfLines = 0
+        adressLabel.numberOfLines = 0
         stateLabel.numberOfLines = 0
         
         contentView.addSubview(stackView)
         stackView.addSubview(placeImageView)
         stackView.addSubview(namePlaceLabel)
         stackView.addSubview(categoryLabel)
+        stackView.addSubview(adressLabel)
+        stackView.addSubview(neighborhoodLabel)
         stackView.addSubview(stateLabel)
+        stackView.addSubview(numberLabel)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: topAnchor, constant: 0),
-            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
-            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
-            stackView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: -20),
             stackView.heightAnchor.constraint(equalToConstant: 400),
             
-            placeImageView.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 8),
-            placeImageView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 8),
-            placeImageView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 8),
+            placeImageView.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 0),
+            placeImageView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 20),
+            placeImageView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: -20),
+            placeImageView.heightAnchor.constraint(equalToConstant: 200),
             
             namePlaceLabel.topAnchor.constraint(equalTo: placeImageView.bottomAnchor, constant: 16),
-            namePlaceLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 8),
+            namePlaceLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 40),
             
             namePlaceLabel.topAnchor.constraint(equalTo: placeImageView.bottomAnchor, constant: 16),
-            namePlaceLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 8),
+            namePlaceLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 40),
             
-            categoryLabel.topAnchor.constraint(equalTo: namePlaceLabel.bottomAnchor, constant: 16),
-            categoryLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 8),
+            categoryLabel.topAnchor.constraint(equalTo: namePlaceLabel.bottomAnchor, constant: 8),
+            categoryLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 40),
             
-            stateLabel.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 32),
-            stateLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 8),
+            adressLabel.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 8),
+            adressLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 40),
+            
+            neighborhoodLabel.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 8),
+            neighborhoodLabel.leadingAnchor.constraint(equalTo: adressLabel.trailingAnchor, constant: 2),
+            
+            stateLabel.topAnchor.constraint(equalTo: adressLabel.bottomAnchor, constant: 2),
+            stateLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 40),
+            
+            numberLabel.topAnchor.constraint(equalTo: stateLabel.bottomAnchor, constant: 2),
+            numberLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 40),
         ])
     }
     
@@ -108,6 +150,9 @@ class PlacesListCell: UITableViewCell {
         self.placeImageView.image = UIImageView(image: UIImage(named: "placeImageDefault")).image
         self.namePlaceLabel.text = data.namePlace
         self.categoryLabel.text = data.category
-        self.stateLabel.text = data.adress
+        self.adressLabel.text = "\(data.adress)" + ","
+        self.neighborhoodLabel.text = data.neighborhood
+        self.stateLabel.text = data.state
+        self.numberLabel.text = "Nº " + "\(data.number)"
     }
 }
--- escoleta/escoleta/Controller/PlacesList/PlacesListViewController.swift
+++ escoleta/escoleta/Controller/PlacesList/PlacesListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class PlacesListViewController: UIViewController {
     //MARK: Variable
+    let cellHeight: CGFloat = 400
     var viewModel = PlacesListViewModel()
     
     //MARK: View Properties
@@ -40,8 +41,6 @@ class PlacesListViewController: UIViewController {
         let tableView = UITableView()
         tableView.backgroundColor = .backgroundColor
         tableView.separatorStyle = .none
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 400
         tableView.register(PlacesListCell.self, forCellReuseIdentifier: PlacesListCell.identifier)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
@@ -50,6 +49,10 @@ class PlacesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         view.backgroundColor = .backgroundColor
         setupLayout()
         setupConstraints()
@@ -59,32 +62,41 @@ class PlacesListViewController: UIViewController {
         view.addSubview(stackView)
         stackView.addSubview(filterPlacesListSearchBar)
         view.addSubview(foundPlacesLabel)
-        //view.addSubview(tableView)
+        view.addSubview(tableView)
     }
     
     fileprivate func setupConstraints() {
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
             stackView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
             stackView.heightAnchor.constraint(equalToConstant: 60),
             
             filterPlacesListSearchBar.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 20),
-            filterPlacesListSearchBar.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 20),
             filterPlacesListSearchBar.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 20),
+            filterPlacesListSearchBar.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 20),
             
             foundPlacesLabel.topAnchor.constraint(equalTo: filterPlacesListSearchBar.bottomAnchor, constant: 20),
             foundPlacesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             
-//            tableView.topAnchor.constraint(equalTo: filterPlacesListSearchBar.bottomAnchor, constant: 0),
-//            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-//            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-//            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+            tableView.topAnchor.constraint(equalTo: foundPlacesLabel.bottomAnchor, constant: 20),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
         ])
     }
 }
 
+//MARK: - UITableViewDelegate
+extension PlacesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cellHeight
+    }
+    
+}
+
+//MARK: - UITableViewDataSource
 extension PlacesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.fetchPlacesList().count
